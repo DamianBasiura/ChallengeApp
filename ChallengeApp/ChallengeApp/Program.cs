f6f57c3e9302e91728b1a8f805c626c7bc5c6bf9
Damian Basiura <kontowbanku@gmail.com>
2023-09-29 17:22:35 +0200

    Zadanie dzień 4

--- ChallengeApp/ChallengeApp/Program.cs
+++ ChallengeApp/ChallengeApp/Program.cs
@@ -1,1 +1,20 @@
-﻿Console.WriteLine("Witaj, Damianie!");
+﻿var name = "Ewa";
+var age = 27;
+var plec = "Kobieta";
+
+
+if (plec == "Kobieta" && age < 30)
+{
+    if (name == "Ewa")
+    { Console.WriteLine("Kobieta poniżej 30 lat, Ewa."); }
+    else
+    {
+        Console.WriteLine("Kobieta poniżej 30 lat, ale nie Ewa.");
+    }
+
+    Console.WriteLine("Jest to na pewno kobieta poniżej 30 lat.");
+}
+else
+{
+    Console.WriteLine("Ktoś inny");
+}
